72aa68de42b4d3e6df8ff71e2ffed3191f804e55
AkinAguda <lordtitan7@gmail.com>
2021-12-02 16:56:00 +0100

    made some progress on bulb

--- src/components/common/LightBulb/LightBulb.tsx
+++ src/components/common/LightBulb/LightBulb.tsx
@@ -4,11 +4,14 @@ import classes from "./LightBulb.module.scss";
 const LightBulb: React.FC = () => (
   <div className={classes.container}>
     <div className={classes.bulb}>
-      <div className={classes.wire} />
+      <div className={classes.wireWrapper}>
+        <div className={classes.wire} />
+      </div>
       <div className={classes.lampHolder} />
       <div className={classes.bulbCurves} />
       <div className={classes.bulbGlass} />
     </div>
+    <div className={classes.glare} />
   </div>
 );
 
